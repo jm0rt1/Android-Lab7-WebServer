703691b99853bd7c51afeadab7a0dd948a6f1105
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-21 16:51:00 -0400

    more changes

--- update_web_app.sh
+++ update_web_app.sh
@@ -4,9 +4,11 @@ set -e
 ./exit-on-uncommitted-changes.sh
 echo "Updating web app"
 WEB_APP_PATH="/Applications/XAMPP/xamppfiles/htdocs/slimapp"
+CURRENT_DIR=pwd
+
 
 index_dest_path="${WEB_APP_PATH}/index.php"
 
 cp ./src/index.php ${index_dest_path}
 
-(cd ${WEB_APP_PATH} && ./init-venv.sh)
+(cd ${WEB_APP_PATH} && "./${CURRENT_DIR}/init-venv.sh")

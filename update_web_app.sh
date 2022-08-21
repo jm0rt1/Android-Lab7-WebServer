7e942cc1a0dbe84aa705e83feb71e7b4cbcdc863
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-21 17:03:13 -0400

    done with scripts maybe

--- update_web_app.sh
+++ update_web_app.sh
@@ -11,11 +11,14 @@ cwd=$(pwd)
 # --- variables --- #
 index_dest_path="${WEB_APP_PATH}/index.php"
 composer_dest_path="${WEB_APP_PATH}/composer.json"
-composer_dest_path="${WEB_APP_PATH}/composer.json"
+htaccess_dest_path="${WEB_APP_PATH}/.htaccess"
 
 
 
 cp ./src/index.php ${index_dest_path}
 cp ./src/composer.json ${composer_dest_path}
+cp ./src/.htaccess ${htaccess_dest_path}
+
+
 
 (cd ${WEB_APP_PATH} && "${cwd}/init-venv.sh")
